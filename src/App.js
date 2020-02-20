2391db7f593029090bee852692ddd0cea1a61e9e
YC <yallappahugar11@gmail.com>
2020-02-20 22:28:21 +0530

    React based wep page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
+import './css/style.css';
+import Navbar from './components/navbar';
+import Home from './components/home';
+import ActorsContainer from './components/actorsContainer';
+import Actress from './components/actress';
+import Films from './components/films';
+// import Actors from './components/actors';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return(
+      <BrowserRouter>
+        <div className="App">
+        <Navbar title="Welcome to My Web Page"/>
+        <Route exact path="/" render={() => <Home title="Hi, I'm YC Hugar !" />} />
+        <Route path="/actors" render={() => <ActorsContainer title="My Best Actor! "/>} />
+        <Route path="/actress" render={() => <Actress title="My Best Actresses! " />} />
+        <Route path="/films" render={() => <Films />} />
+        {/* <Route path="/actors" render={() => <Actors title="My Best Actor! "/>} /> */}
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-
 export default App;
